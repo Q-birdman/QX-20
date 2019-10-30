99f7327a06066cf1e2b14097df1e7ab5b73f9bfe
GoshBird <daitaka5606@gmail.com>
2019-10-30 19:22:22 +0900

    no.4のメインプログラムです。

---
+++ Arduino講座/No.4/servoProg/servoProg.ino
@@ -0,0 +1,19 @@
+#include<Servo.h>
+
+Servo myservo;
+
+void setup() {
+  myservo.attach();
+
+}
+
+void loop() {
+  myservo.write(120);
+  delay(1000);
+  
+  myservo.write(90);
+  delay(1000);
+  
+  myservo.write(60);
+  delay(1000);
+}
