90613e2841c6b5d7191aa614531b9d454367e16d
taniste <57521168+taniste@users.noreply.github.com>
2019-11-08 16:59:27 +0900

    光度

---
+++ sketch_nov06a/sketch_nov06a.ino
@@ -0,0 +1,22 @@
+#define LED 2
+#define sensor 0
+int val = 0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val = analogRead(sensor);
+//Serial.println(val);
+if(val <= 100){
+  Serial.println("暗いわ");
+}
+else if(val <= 200){
+  Serial.println("あー、まあまあです。");
+}
+else{
+  Serial.println("明るすぎん?");
+}
+}
